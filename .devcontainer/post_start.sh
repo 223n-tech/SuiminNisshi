5fb9b6acf20f43779748d20a6178b7311ceb6e6d
wate <pc.poisoning@gmail.com>
2025-02-24 20:10:28 +0900

    ローカル開発環境の設定を調整

--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 # フォルダの管理者がrootになってしまう問題に対応
-# sudo chown $(whoami):$(whoami) -R .
+sudo chown $(whoami):$(whoami) -R .
 
-# if [ ! -e "${PWD}/composer.lock" ]; then
-#   composer install -n
-# fi
+## Apacheのrewriteモジュールを有効化
+if [ ! -e "/etc/apache2/mods-enabled/rewrite.load" ]; then
+  a2enmod rewrite
+fi
+
+if [ ! -e "${PWD}/composer.lock" ]; then
+  composer install -n
+fi
+
+## app_local.phpが存在しない場合はpost-install-cmdを実行
+if [ ! -e "${PWD}/config/app_local.php" ]; then
+  composer run post-install-cmd
+fi
 
 if [ -z "${__GIT_PROMPT_SHOW_CHANGED_FILES_COUNT}" ]; then
   source "${HOME}/.bashrc"
